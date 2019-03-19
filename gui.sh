0bc122d6aa785e89f564585ac3bad502e6a06751
golan <golan@localhost.localdomain>
2019-03-19 10:37:56 -0400

    add valid

--- gui.sh
+++ gui.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+## This Program needs root or user with permissions to use yum
+
+f_chkfordia(){
+
+sudo yum list installed -q 'dialog'
+ if [ $? == 0 ]; then
+      echo "dialog is installed"
+ else echo "dialog requierd for running"
+      read -p "Download & Install? y/n " ans
+
+     if [ $ans == y ]; then 
+       sudo yum install -y -q dialog
+     else echo "Canceling program" 
+          exit
+     fi
+ fi
+}
+
+f_chkfordia
+
 dialog --title "Program" --menu "Choose an option" 20 60 18 "ADD Student" "Adding a student" "Delete Studant" "Deleting a student" "ADD Grade" "Adding a grade" "Student Avg" "Calculating Student Avg" "Max Avg" "The student with the highest avg" "Replace Grades" "Replacing grades for two students"
 
 clear
